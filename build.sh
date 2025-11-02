3d9d0766c3318835c2c90eaa8ebecdba72ec1253
bcp <example@example.com>
2025-11-01 22:28:43 -0400

    added build script

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -e
+
+BOARD=nice_nano_v2
+SHIELD_LEFT=corne_left
+SHIELD_RIGHT=corne_right
+ZMK_DIR=../zmk
+
+if ! docker info > /dev/null 2>&1; then
+    echo "Docker is not running. Starting Docker..."
+    open -a Docker
+    echo "Waiting for Docker to start..."
+    while ! docker info > /dev/null 2>&1; do
+        sleep 1
+    done
+    echo "Docker is now running."
+fi
+
+docker run --rm -it \
+  -v "$(pwd)":/workdir/zmk-config \
+  -v "$(pwd)/${ZMK_DIR}":/workdir/zmk \
+  -w /workdir/zmk-config \
+  zmkfirmware/zmk-dev-x86_64:4.1-branch \
+  west build -s /workdir/zmk/app -d build/left -b $BOARD -- \
+    -DSHIELD=$SHIELD_LEFT \
+    -DZMK_CONFIG=/workdir/zmk-config/config
+
+docker run --rm -it \
+  -v "$(pwd)":/workdir/zmk-config \
+  -v "$(pwd)/${ZMK_DIR}":/workdir/zmk \
+  -w /workdir/zmk-config \
+  zmkfirmware/zmk-dev-x86_64:4.1-branch \
+  west build -s /workdir/zmk/app -d build/right -b $BOARD -- \
+    -DSHIELD=$SHIELD_RIGHT \
+    -DZMK_CONFIG=/workdir/zmk-config/config
